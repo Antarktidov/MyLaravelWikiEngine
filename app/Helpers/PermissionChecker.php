f097b98e8709704f2470c04114deb05fff8cdb18
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2025-06-08 20:18:22 +0300

    добавлены комментарии в хелпер PermissionChecker.php

--- app/Helpers/PermissionChecker.php
+++ app/Helpers/PermissionChecker.php
@@ -13,67 +13,100 @@ use App\Models\Wiki;
 use App\Models\UserGroup;
 use App\Models\UserUserGroupWiki;
 
+/**
+ * Класс PermissionChecker - это хелпер (помощник), который позволяет нам проверить,
+ * имеет ли пользователь какое-либо техническое право на данной вики или глобально
+*/
 class PermissionChecker
 {
+    /**
+     * Данная функция предназначена для вызова в файле AppServiceProvider.php
+     * Принимаемые параметры: пользователь, имя-url вики, техническое право
+     */
     public static function check($user, $wikiName, $permission)
     {
         $wiki = DB::table('wikis')->where('url', $wikiName)->first();
+        //ищем вики по url
         if ($wiki) {
+            //получаем права на вики
             $user_user_group_wiki = self::getUserGroupWiki($user, $wiki);
         } else {
+            //получаем права вне вики (для управления вики-фермой)
             $user_user_group_wiki = self::getUserGroup($user);
         }
-        $user_groups = UserGroup::all();
 
+        $user_groups = UserGroup::all();//получаем все группы
+
+        /**
+         * Ищем, где сущесвующая группа (на вики или глобально) совпадает с
+         * группой участника.
+         */
         foreach ($user_user_group_wiki as $user_user_group_wiki_foreach) {
             foreach ($user_groups as $user_group) {
                 if ($user_user_group_wiki_foreach->user_group_id === $user_group->id) {
                     if ($user_group->$permission === 1) {
                         return true;
+                        /**
+                         * Если у группы, которой владеет участник,
+                         * есть техническое право, то участник имеет техническое право...
+                         */
                     }
                 }
             }
         }
-        return false;
+        return false;//... в противном случае он его не имеет
     }
 
+    /**
+     * Данная функция предназвачена для вызова в Middleware.
+     */
     public static function check_for_middleware($request, $next, $permission): Response
-{
-    $user = auth()->user();
+    {
+        $user = auth()->user();
 
-    if ($user) {
-        $wikiName = $request->route('wikiName');
-        $wiki = DB::table('wikis')->where('url', $wikiName)->first();
-        if ($wiki) {
-            $user_user_group_wiki = self::getUserGroupWiki($user, $wiki);
-            //dd('Группы юзера получены ВМЕСТЕ вики');
-        } else {
-            $user_user_group_wiki = self::getUserGroup($user);
-            //dd('Группы юзера получены БЕЗ вики');
-        }
-            //dd('До итерации');
-            $user_groups = UserGroup::all();
-            foreach ($user_user_group_wiki as $user_user_group_wiki_foreach) {
-                foreach ($user_groups as $user_group) {
-                    //dd('foreach ($user_groups as $user_group итерация 1');
-                    if ($user_user_group_wiki_foreach->user_group_id === $user_group->id) {
-                        if ($user_group->$permission === 1) {
-                            return $next($request);
+        if ($user) {
+            $wikiName = $request->route('wikiName');//получаем url вики
+            $wiki = DB::table('wikis')->where('url', $wikiName)->first(); //получем вики по url
+            if ($wiki) {
+                //получаем права на вики
+                $user_user_group_wiki = self::getUserGroupWiki($user, $wiki);
+            } else {
+                //получаем права на вики-ферме
+                $user_user_group_wiki = self::getUserGroup($user);
+            }
+                $user_groups = UserGroup::all();//получаем все группы
+
+                /**
+                 * Ищем, где сущесвующая группа (на вики или глобально) совпадает с
+                 * группой участника.
+                 */
+                foreach ($user_user_group_wiki as $user_user_group_wiki_foreach) {
+                    foreach ($user_groups as $user_group) {
+                        
+                        if ($user_user_group_wiki_foreach->user_group_id === $user_group->id) {
+                            if ($user_group->$permission === 1) {
+                                return $next($request);
+                                /**
+                                * Если у группы, которой владеет участник,
+                                * есть техническое право, то участник имеет техническое право.
+                                */
+                            }
                         }
                     }
-                }
-            //}
 
-            return response('Forbidden', 403);
-        } /*else {
-            return response('Wiki not found', 404);
-        }*/
-    } else {
-        return response('Unauthorized', 401);
+                return response('Forbidden', 403);
+            }
+        } else {
+            return response('Unauthorized', 401);
+        }
+        return response('Forbidden', 403);
     }
-    return response('Forbidden', 403);
-}
 
+    /**
+     * Приватная функция для получения прав участника на конкретной вики
+     * или глобальный прав. wiki_id 0 - магичсекая константа для глобальных групп
+     * (групп, не привязанных к вики)
+     */
     private static function getUserGroupWiki($user, $wiki)
     {
         return DB::table('user_user_group_wiki')
@@ -84,6 +117,10 @@ class PermissionChecker
             })
             ->get();
     }
+    /**
+     * Приватная функция для получения прав участников вне вики. Рабоатет только
+     * для глобальных групп
+     */
 
     private static function getUserGroup($user)
     {
