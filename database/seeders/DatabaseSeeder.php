67467984d9b7c82c1f8596b59ef6aa3a8c21c77d
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2025-08-17 23:48:43 +0300

    Замена DB фасадов на equalent ORM

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\UserGroup;
+use App\Models\Wiki;
+use App\Models\Article;
+use App\Models\Revision;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -23,7 +26,7 @@ class DatabaseSeeder extends Seeder
 
         //Creating global user groups
         //Steward
-        DB::table('user_groups')->insert([
+        UserGroup::create([
             'name' => 'steward',
             'is_global' => 1,
             'can_create_articles' => 1,
@@ -45,7 +48,7 @@ class DatabaseSeeder extends Seeder
 
         //Creating local user groups
         //Admin    
-        DB::table('user_groups')->insert([
+        UserGroup::create([
             'name' => 'admin',
             'is_global' => 0,
             'can_create_articles' => 1,
@@ -66,26 +69,27 @@ class DatabaseSeeder extends Seeder
         ]);
 
         //Creating your first wiki
-        DB::table('wikis')->insert([
+        $wiki = Wiki::create([
             'url' => 'my-first-wiki',
         ]);
 
         //Creating your first article in your first wiki
-        DB::table('articles')->insert([
-            'wiki_id' => 1,//it's your first wiki id
+        $article = Article::create([
+            'wiki_id' => $wiki->id,
             'title' => 'My first article',
             'url_title' => 'my-first-article',
         ]);
 
         //Creating your first revision for your first article
-        DB::table('revisions')->insert([
-            'article_id' => 1,//it's your first article id
+        Revision::create([
+            'article_id' => $article->id,
             'title' => 'My first article',
             'url_title' => 'my-first-article',
             'content' => 'Hello, this is content of your first article! Edit it!',
             'user_ip' => '127.0.0.1',//localhost
             'user_id' => 0,
         ]);
+        
         echo 'Installation completed! Don\'t forget to register and grant steward rights to your account via database editor!';
     }
 }
